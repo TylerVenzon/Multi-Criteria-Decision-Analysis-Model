f8d7015f811cf26a62d75c33eec4c8e505d4c556
DESKTOP-RMKDPDI\Tyler <justin_venzon@dlsu.edu.ph>
2018-03-15 15:12:29 +0800

    Updated classifier

--- src/classifier/classifier.py
+++ src/classifier/classifier.py
@@ -10,20 +10,33 @@ class Classifier:
         
         optimizer = Optimizer()
         
-        if index <= 0:
-            return -1
-        
-        elif index <= optimizer.HIGH_HAZARD_THRESHOLD:
-            return optimizer.HIGH_HAZARD_INDEX  
-        
-        elif index <= optimizer.MEDIUM_HAZARD_THRESHOLD: 
-            return optimizer.MEDIUM_HAZARD_INDEX
-        
-        elif index <= optimizer.LOW_HAZARD_THRESHOLD:
-            return optimizer.LOW_HAZARD_INDEX
-        
-        else:
+        if index >= optimizer.SAFE_THRESHOLD:
             return optimizer.SAFE_INDEX
+        elif index >= optimizer.LOW_HAZARD_THRESHOLD:
+            return optimizer.LOW_HAZARD_INDEX
+        elif index >= optimizer.MEDIUM_HAZARD_THRESHOLD:
+            return optimizer.MEDIUM_HAZARD_INDEX
+        elif index >= optimizer.HIGH_HAZARD_THRESHOLD:
+            return optimizer.HIGH_HAZARD_INDEX
+        else:
+            return optimizer.UNSAFE_INDEX
+        
+# =============================================================================
+#         if index <= 0:
+#             return -1
+#           
+#         elif index <= optimizer.HIGH_HAZARD_THRESHOLD:
+#             return optimizer.HIGH_HAZARD_INDEX  
+#         
+#         elif index <= optimizer.MEDIUM_HAZARD_THRESHOLD: 
+#             return optimizer.MEDIUM_HAZARD_INDEX
+#         
+#         elif index <= optimizer.LOW_HAZARD_THRESHOLD:
+#             return optimizer.LOW_HAZARD_INDEX
+#         
+#         else:
+#             return optimizer.SAFE_INDEX
+# =============================================================================
         
     
                 
